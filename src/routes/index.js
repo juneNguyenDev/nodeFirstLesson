95aae896cb4473bfc33e3c27672c75caf280a324
juneNguyenDev <junenene2906@gmail.com>
2023-06-18 10:44:12 +0700

    message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
 function route(app) {
-    app.use('/news', newsRouter);
+             app.use('/news', newsRouter);
 
     app.use('/', siteRouter);
 }
