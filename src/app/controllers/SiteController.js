0ef69f592c5d82155fa9f7333c504b66d8b37403
juneNguyenDev <junenene2906@gmail.com>
2023-06-20 22:55:56 +0700

    Hi

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,11 +1,16 @@
+const Course = require('../models/Course')
 class SiteController {
+    
     index(req, res) {
-        res.render('home');
-    }
+        // res.render('home');
+        Course.find({})
+        .then(courses => {res.json(courses)})
+        .catch(err => {res.status(400).json({error:"Loi Mat ROi"})})
+    };
     //Get slug :
     // Search
     search(req, res) {
         res.render('search');
     }
-}
+};
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
-var morgan = require('morgan');
+const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000;
-const route = require('./routes/index');
+const route = require('./routes');
 app.use(morgan('combined'));
+const db = require('./config/db')
 
+// connect 
+db.connect();
 //Template engine
 app.engine(
     'hbs',
@@ -16,8 +18,10 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources','views'));
 route(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
